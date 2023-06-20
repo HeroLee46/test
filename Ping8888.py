83b9887cde79962ad04d7df8a300b38dceda6df4
HeroLee46 <97429891+HeroLee46@users.noreply.github.com>
2023-06-20 17:01:04 +0800

    Update Ping8888.py
    8.8.8.8 to www.google.com.tw

--- Ping8888.py
+++ Ping8888.py
@@ -2,7 +2,7 @@ import subprocess
 
 def ping_google_dns():
     try:
-        result = subprocess.run(['ping', '-n', '4', '8.8.8.8'], capture_output=True, text=True, check=True)
+        result = subprocess.run(['ping', '-n', '4', 'www.google.com.tw'], capture_output=True, text=True, check=True)
         return result.stdout
     except subprocess.CalledProcessError as e:
         return e.stderr
